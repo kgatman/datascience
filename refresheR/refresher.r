cb394c976252bc2fa05d8d37a1e66de7b556a7b4
kgatman <makhatemakhate@gmail.com>
2024-08-15 14:13:41 +0200

    refreshing

--- refresheR/refresher.r
+++ refresheR/refresher.r
@@ -1,5 +1,13 @@
+#I'm playing around with Positron a lot in lieu of RStudio 
+
 #Vectors
 MyFirstVector <- c(1,2,3,4,10,20,30,123)
 is.integer(MyFirstVector)
 is.double(MyFirstVector)
 is.numeric(MyFirstVector)
+onetofif<-seq(1,15)
+repp <- rep("Makhate",2)
+repp[2]
+a<-c(50,34,111,7,24,631,20,4,7,21)
+b<-c(100,2,56,12,0,65,93,10,244,1)
+?rnorm()
