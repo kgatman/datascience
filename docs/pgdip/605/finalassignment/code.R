75c734fb2dcb0bb016f8e46d28c2d69af816468e
kgatman <makhatemakhate@gmail.com>
2024-10-12 11:42:56 +0200

    html files

---
+++ docs/pgdip/605/finalassignment/code.R
@@ -0,0 +1,8 @@
+### Question 1
+### Importing the excel dataset into a dataframe
+
+setwd("C:\Users\Makhate\deepdives\datascience\docs\pgdip\605\finalassignment\CO2EmissionsData.xlsx")
+
+CO2data <- read.csv("CO2EmissionsData.csv")
+
+str(CO2data)
